26f4c933d5ec521143a87f25b3c5ddfde215a5f2
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-13 20:37:51 +0300

    Setting up entity framework

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
