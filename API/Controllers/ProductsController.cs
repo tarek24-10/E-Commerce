b0c3945fd5aec25b6c26a49e5d98113f3cb1a09e
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-17 18:15:59 +0300

    Use Projection to query Brands & Types

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -84,13 +84,15 @@ namespace API.Controllers
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
         {
-            return Ok();
+            var spec = new BrandListSpecification();
+            return Ok(await repo.ListAllWithSpecAsync(spec));
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
         {
-            return Ok();
+            var spec = new TypeListSpecification();
+            return Ok(await repo.ListAllWithSpecAsync(spec));
         }
     }
 }
