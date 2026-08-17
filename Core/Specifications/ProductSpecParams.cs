6876abf167ca09abfcf79dd6e0f0dab092b7d084
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-17 20:56:39 +0300

    Add the Search Functionality

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -29,5 +29,12 @@
         }
 
         public string? Sort { get; set;  }
+
+        private string? _search;
+        public string Search
+        {
+            get => _search ?? "";
+            set => _search = value.ToLower();
+        }
     }
 }
--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -5,6 +5,7 @@ namespace Core.Specifications
     public class ProductSpecification : BaseSpecification<Product>
     {
         public ProductSpecification(ProductSpecParams specParams) : base( p => 
+                        (string.IsNullOrEmpty(specParams.Search) || p.Name.ToLower().Contains(specParams.Search)) &&
                         (specParams.Brands.Count == 0 || specParams.Brands.Contains(p.Brand)) &&
                         (specParams.Types.Count == 0 || specParams.Types.Contains(p.Type))
             )
