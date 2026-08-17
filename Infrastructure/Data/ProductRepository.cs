e412ce7548fd629dea40fb90b97095c8e24f4136
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-17 13:17:20 +0300

    Filter Products by Brand & Type

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -28,9 +28,21 @@ namespace Infrastructure.Data
             return await context.Products.FindAsync(id);
         }
 
-        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        public async Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type)
         {
-            return await context.Products.AsNoTracking().ToListAsync();
+            var query = context.Products.AsNoTracking().AsQueryable();
+
+            if (!string.IsNullOrEmpty(brand))
+            {
+                query = query.Where(p => p.Brand == brand);
+            }
+
+            if (!string.IsNullOrEmpty(type))
+            {
+                query = query.Where(p => p.Type == type);
+            }
+
+            return await query.ToListAsync();
         }
 
         public async Task<IReadOnlyList<string>> GetTypesAsync()
