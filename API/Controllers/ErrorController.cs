167bffbba3ff8b5e958c4e4fbc101d5677d0cd22
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-18 01:36:50 +0300

    Adding a test Controller for Error Handling

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,40 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet("Unauthorized")]
+        public IActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("NotFound")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("BadRequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("Not a good request");
+        }
+
+        [HttpGet("InternalError")]
+        public IActionResult GetInternalError()
+        {
+            throw new Exception("This is a test exception");
+        }
+
+        [HttpPost("ValidationError")]
+        public IActionResult GetValidationError(Product product)
+        {
+            return BadRequest("Validation failed");
+        }
+    }
+}
