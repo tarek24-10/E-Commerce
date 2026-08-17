3a1f29c10c1094bd4a0c519a6b1166f7a5839c7c
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-18 01:37:12 +0300

    Create Exception Handling Middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middlewares;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Data.SeedData;
@@ -18,6 +19,8 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenenricRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
