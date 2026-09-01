1acff81b8e3d5534bcb9b0ec9970dbd92e47bee3
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-02 01:59:36 +0300

    Create an Authentication Test endpoint

--- API/Controllers/ErrorController.cs
+++ API/Controllers/ErrorController.cs
@@ -1,6 +1,8 @@
 ﻿using API.DTOs;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -36,6 +38,15 @@ namespace API.Controllers
         public IActionResult GetValidationError(ProductDto product)
         {
             return BadRequest("Validation failed");
+        }
+
+        [Authorize]
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            var name = User.FindFirst(ClaimTypes.Name)?.Value;
+            var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return Ok("Hello " + name + " with the id of " + id);
         }
     }
 }
