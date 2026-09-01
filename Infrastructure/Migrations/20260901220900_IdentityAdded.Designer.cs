d0be39b00e3e8f0f563e6cc8db1c8751b1a899a2
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-02 01:22:07 +0300

    Updating the DB & Testing the Endpoints

--- Infrastructure/Migrations/20260901220900_IdentityAdded.Designer.cs
+++ Infrastructure/Migrations/20260901220900_IdentityAdded.Designer.cs
@@ -4,16 +4,19 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Infrastructure.Migrations
+namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20260901220900_IdentityAdded")]
+    partial class IdentityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
