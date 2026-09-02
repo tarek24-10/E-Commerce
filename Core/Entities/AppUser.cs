5e0e8061b59d0561db1734706b564bb5986307e9
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-02 13:41:18 +0300

    Adding a User Address Entity

--- Core/Entities/AppUser.cs
+++ Core/Entities/AppUser.cs
@@ -6,5 +6,6 @@ namespace Core.Entities
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public Address? Address { get; set; } 
     }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,11 +1,14 @@
 ﻿using Core.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Infrastructure.Data
 {
     public class StoreContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<Address> Address { get; set; }
+
     }
 }
