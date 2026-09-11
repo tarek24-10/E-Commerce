4f511d5a67eecacb5d2bd59ca9bc5295a6296f56
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-12 01:52:02 +0300

    Creating the Order Aggregate

---
+++ Core/Entities/OrderAggregate/ProductItemordered.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities.OrderAggregate
+{
+    public class ProductItemordered
+    {
+        public int ProductId { get; set; }
+        public required string ProductName { get; set; }
+        public required string PictureUrl { get; set; }
+    }
+}
